fedeb90b4336bceeba6230ddcb5619152de9f21c
jarganaraz85 <106669406+jarganaraz85@users.noreply.github.com>
2024-10-02 17:26:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+use Pyrus\utils\Pyrus;
 // Set the appropriate headers for JSON response
 header("Content-Type: application/json");
 
@@ -11,13 +11,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Convert the JSON data to a PHP associative array
     $received_data = json_decode($raw_data, true);
 
-    // Return the received data as the response
-    var_dump($received_data);
-    echo json_encode($received_data);
+    // Create an instance of the Pyrus class
+
+    $pyrusCredentials =    [
+      'login' => "ecitestadm@outlook.com",
+      'security_key' => "xYUsC~6Sf7N4JfbmPBIT7-nIWRKavULwDMiPHxG81qw0R6aBxJ1mIuEnXicSIPiTWQRwgJo-2IIYDjvp0HNOzeR8W1A~cFtD"
+    ];
+    $pyrus = new Pyrus($pyrusCredentials);
+    try {
+      // Call the getFieldByName method
+      $fieldStatus = $pyrus->getFieldByName(1452357, "ECI Invoice Export Status");
+
+      // Call the setPyrusWorkflowApprovalsChoice method
+      setPyrusWorkflowApprovalsChoice([$received_data["task"]], "approved", [
+          $fieldStatus['id'] => "CFR Test",
+      ], $pyrus);
+      // Return the received data as the response    
+    //echo json_encode($received_data);
+    } catch (Exception $e) {
+      http_response_code(500); // Internal Server Error
+      echo json_encode(array("error" => $e->getMessage()));
+    }
 } else {
     // Return an error message for unsupported request methods
     http_response_code(405); // Method Not Allowed
     echo json_encode(array("error" => "Method not allowed."));
+}
+
+
+function setPyrusWorkflowApprovalsChoice($registers, $choice, $fieldsToUpdate, $pyrus) {
+  $result = [];
+  foreach ($registers as $register) {
+    $result[] = $pyrus->setWorkflowApprovalChoice(
+      $register["id"],
+      $choice,
+      $fieldsToUpdate
+    );
+  }
+  return $result;
 }
 
 ?>
